b6556158926df528a3d0a56420a4261595efbfda
Anhelina <zhmurkevych447@gmail.com>
2026-04-06 20:40:40 +0300

    before deps fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { Collection } from 'mongoose';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: AuthModule,
+        schemaOptions: {
+          collection: 'Auth'
+        }
+      }
+    ])
+  ]
 })
 export class AuthModule {}
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ReviewService } from './review.service';
+import { ReviewModel } from './review.model';
 
 @Module({
-  controllers: [ReviewController]
+  controllers: [ReviewController],
+  imports: [
+      TypegooseModule.forFeature([
+        {
+          typegooseClass: ReviewModel,
+          schemaOptions: {
+            collection: 'review'
+          }
+        }
+      ])
+    ],
+  providers: [ReviewService],
+  exports: [ReviewService]
 })
 export class ReviewModule {}
