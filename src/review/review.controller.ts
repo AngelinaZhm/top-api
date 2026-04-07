f860fba37ddf71ca8730968cf1754a4dd53ee0b2
Anhelina <zhmurkevych447@gmail.com>
2026-04-07 22:12:18 +0300

    46

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,6 +15,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -36,7 +37,7 @@ export class ReviewController {
 	}
 
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {
+	async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
 		return this.reviewService.findByProductId(productId);
 	}
 }
